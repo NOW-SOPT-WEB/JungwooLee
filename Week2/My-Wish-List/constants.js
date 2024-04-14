ec86131da9ee7cad5e3b17a923bb2c14534be012
jungwoo3490 <jungwoo3490@naver.com>
2024-04-15 02:02:34 +0900

    feat: constant 데이터 활용하여 상품 목록 렌더링 구현

---
+++ Week2/My-Wish-List/constants.js
@@ -0,0 +1,62 @@
+export const shoppingList = [
+  {
+    img: "./assets/png/img_digital_camera.jpg",
+    name: "레트로 카메라",
+    price: 34900,
+    category: "digital_item",
+  },
+  {
+    img: "./assets/png/img_airpod_max.png",
+    name: "에어팟 맥스",
+    price: 709000,
+    category: "digital_item",
+  },
+  {
+    img: "./assets/png/img_speaker.png",
+    name: "블루투스 스피커",
+    price: 1850000,
+    category: "digital_item",
+  },
+  {
+    img: "./assets/png/img_iMac.png",
+    name: "iMac",
+    price: 22900000,
+    category: "digital_item",
+  },
+  {
+    img: "./assets/png/img_chocolate.png",
+    name: "레더라 초콜릿",
+    price: 60000,
+    category: "food_item",
+  },
+  {
+    img: "./assets/png/img_mozzi.png",
+    name: "과일모찌",
+    price: 23900,
+    category: "food_item",
+  },
+  {
+    img: "./assets/png/img_rollcake.png",
+    name: "롤케이크",
+    price: 15900,
+    category: "food_item",
+  },
+  {
+    img: "./assets/png/img_bodypillow.png",
+    name: "오리 바디필로우",
+    price: 12900,
+    category: "living_item",
+  },
+  {
+    img: "./assets/png/img_mood_lamp.png",
+    name: "무드등",
+    price: 132000,
+    category: "living_item",
+  },
+  {
+    img: "./assets/png/img_book.png",
+    name: "애프터 다크",
+    price: 14200,
+    category: "living_item",
+  },
+];
---
+++ Week2/My-Wish-List/main.js
@@ -0,0 +1,50 @@
+import { shoppingList } from "./constants.js";
+
+shoppingList.forEach(function (item) {
+  console.log(item);
+
+  const sectionItems = document.querySelector(".section_items");
+
+  const sectionItem = document.createElement("div");
+  sectionItem.className = "section_item " + item.category;
+
+  const img = document.createElement("img");
+  img.src = item.img;
+  img.alt = item.name + " 이미지";
+  img.className = "section_item_img";
+
+  const heartButton = document.createElement("button");
+  heartButton.type = "button";
+  heartButton.className = "heart_button";
+  const heartSVG = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
+  heartSVG.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  heartSVG.setAttribute("viewBox", "0 0 512 512");
+  const heartPath = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  heartPath.setAttribute(
+    "d",
+    "M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
+  );
+  heartSVG.appendChild(heartPath);
+  heartButton.appendChild(heartSVG);
+
+  const productName = document.createElement("p");
+  productName.textContent = item.name;
+  productName.className = "product_name";
+
+  const productPrice = document.createElement("p");
+  productPrice.textContent = item.price.toLocaleString();
+  productPrice.className = "product_price";
+
+  sectionItem.appendChild(img);
+  sectionItem.appendChild(heartButton);
+  sectionItem.appendChild(productName);
+  sectionItem.appendChild(productPrice);
+
+  sectionItems.appendChild(sectionItem);
+});
