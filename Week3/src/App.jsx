090403d98459f05c5114accd123d6f8b703945d2
jungwoo3490 <jungwoo3490@naver.com>
2024-04-26 23:31:50 +0900

    chore: Router 초기세팅

--- Week3/src/App.jsx
+++ Week3/src/App.jsx
@@ -1,5 +1,18 @@
+import { Router } from "react-router-dom";
+import styled from "styled-components";
+import GlobalStyle from "../styles/GlobalStyle";
+
 function App() {
-  return <></>;
+  return (
+    <Wrapper>
+      <GlobalStyle />
+      <Router />
+    </Wrapper>
+  );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  height: 100vh;
+`;
--- Week3/src/main.jsx
+++ Week3/src/main.jsx
@@ -1,11 +1,4 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import GlobalStyle from "../styles/GlobalStyle.js";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.Fragment>
-    <GlobalStyle />
-    <App />
-  </React.Fragment>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
