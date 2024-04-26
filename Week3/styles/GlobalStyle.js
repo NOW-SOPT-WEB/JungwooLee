09e5fb8a418d487b27fd098c116d981d8a3a3133
jungwoo3490 <jungwoo3490@naver.com>
2024-04-27 01:10:25 +0900

    fix: GlobalStyle 수정 & 폰트 적용

--- Week3/styles/GlobalStyle.js
+++ Week3/styles/GlobalStyle.js
@@ -1,67 +1,158 @@
 import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+    font-family: 'NPSfontBold';
+    src: url('../src/assets/fonts/NPSfontBold/NPSfontBold.woff2') format('woff2');
+    }
 
-    ${reset}
     * {
-        box-sizing: border-box;
-
-        font-family:
-        'Pretendard Variable',
-        Pretendard,
-        -apple-system,
-        BlinkMacSystemFont,
-        system-ui,
-        Roboto,
-        'Helvetica Neue',
-        'Segoe UI',
-        'Apple SD Gothic Neo',
-        'Noto Sans KR',
-        'Malgun Gothic',
-        'Apple Color Emoji',
-        'Segoe UI Emoji',
-        'Segoe UI Symbol',
-        sans-serif;
+    box-sizing: border-box;
+    font-family: 'NPSfontBold';
     }
 
     html,
+    body,
+    div,
+    span,
+    applet,
+    object,
+    iframe,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    blockquote,
+    pre,
+    a,
+    abbr,
+    acronym,
+    address,
+    big,
+    cite,
+    code,
+    del,
+    dfn,
+    em,
+    img,
+    ins,
+    kbd,
+    q,
+    s,
+    samp,
+    small,
+    strike,
+    strong,
+    sub,
+    sup,
+    tt,
+    var,
+    b,
+    u,
+    i,
+    center,
+    dl,
+    dt,
+    dd,
+    ol,
+    ul,
+    li,
+    fieldset,
+    form,
+    label,
+    legend,
+    table,
+    caption,
+    tbody,
+    tfoot,
+    thead,
+    tr,
+    th,
+    td,
+    article,
+    aside,
+    canvas,
+    details,
+    embed,
+    figure,
+    figcaption,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    output,
+    ruby,
+    section,
+    summary,
+    time,
+    mark,
+    audio,
+    video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 62.5%;
+    /* font: inherit; */
+    vertical-align: baseline;
+    }
+    /* HTML5 display-role reset for older browsers */
+    article,
+    aside,
+    details,
+    figcaption,
+    figure,
+    footer,
+    header,
+    hgroup,
+    menu,
+    nav,
+    section {
+    display: block;
+    }
     body {
-        box-sizing: border-box; /* default */
-        margin: 0 auto;
-
-        font-size: 62.5%;
+    line-height: 1;
     }
-
-    input {
-        outline: none;
+    ol,
+    ul {
+    list-style: none;
     }
-
-    a {
-        color: inherit;
-        text-decoration: none;
-
-        cursor: pointer;
+    blockquote,
+    q {
+    quotes: none;
+    }
+    blockquote:before,
+    blockquote:after,
+    q:before,
+    q:after {
+    content: "";
+    content: none;
+    }
+    table {
+    border-collapse: collapse;
+    border-spacing: 0;
     }
 
     button {
-        font: inherit;
-
-        background: none;
-        cursor: pointer;
-        border: none;
-        padding: 0;
+    border: none;
+    width: 0;
+    height: 0;
+    padding: 0;
+    background-color: transparent;
     }
 
-    select {
-        cursor: pointer;
+    button:hover {
+    cursor: pointer;
     }
 
-    @font-face {
-        font-family: Pretendard;
-        src: url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css')
-        format('woff');
+    a {
+    color: black;
+    text-decoration: none;
     }
+
 `;
 
 export default GlobalStyle;
