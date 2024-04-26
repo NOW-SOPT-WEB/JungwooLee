5ed659a7b8bcb0b2ec487343f670ffab11de4351
jungwoo3490 <jungwoo3490@naver.com>
2024-04-26 23:22:57 +0900

    chore: GlobalStyle 초기세팅

--- Week3/src/main.jsx
+++ Week3/src/main.jsx
@@ -1,4 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+import GlobalStyle from "../styles/GlobalStyle.js";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>
+);
